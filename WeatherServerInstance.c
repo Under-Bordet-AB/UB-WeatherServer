2495b6108f506fa1ae8bee6b2810c03c14257ea5
Patrik Johansson <Patrik.Viby@gmail.com>
2025-11-05 10:10:38 +0100

    added functions for returning mock data from server

--- WeatherServerInstance.c
+++ WeatherServerInstance.c
@@ -1,125 +1,120 @@
 #include "WeatherServerInstance.h"
 #include <stdlib.h>
 
-
-#include "utils.h"
-
 //-----------------Internal Functions-----------------
 
-int WeatherServerInstance_OnRequest(void* _Context);
+int WeatherServerInstance_OnRequest(void *_Context);
 
 //----------------------------------------------------
 
-int WeatherServerInstance_Initiate(WeatherServerInstance* _Instance, HTTPServerConnection* _Connection)
-{
-	_Instance->connection = _Connection;
+int WeatherServerInstance_Initiate(WeatherServerInstance *_Instance,
+                                   HTTPServerConnection *_Connection) {
+  _Instance->connection = _Connection;
 
-	HTTPServerConnection_SetCallback(_Instance->connection, _Instance, WeatherServerInstance_OnRequest);
+  HTTPServerConnection_SetCallback(_Instance->connection, _Instance,
+                                   WeatherServerInstance_OnRequest);
 
-	return 0;
+  return 0;
 }
 
-int WeatherServerInstance_InitiatePtr(HTTPServerConnection* _Connection, WeatherServerInstance** _InstancePtr)
-{
-	if(_InstancePtr == NULL)
-		return -1;
+int WeatherServerInstance_InitiatePtr(HTTPServerConnection *_Connection,
+                                      WeatherServerInstance **_InstancePtr) {
+  if (_InstancePtr == NULL)
+    return -1;
 
-	WeatherServerInstance* _Instance = (WeatherServerInstance*)malloc(sizeof(WeatherServerInstance));
-	if(_Instance == NULL)
-		return -2;
+  WeatherServerInstance *_Instance =
+      (WeatherServerInstance *)malloc(sizeof(WeatherServerInstance));
+  if (_Instance == NULL)
+    return -2;
 
-	int result = WeatherServerInstance_Initiate(_Instance, _Connection);
-	if(result != 0)
-	{
-		free(_Instance);
-		return result;
-	}
+  int result = WeatherServerInstance_Initiate(_Instance, _Connection);
+  if (result != 0) {
+    free(_Instance);
+    return result;
+  }
 
-	*(_InstancePtr) = _Instance;
+  *(_InstancePtr) = _Instance;
 
-	return 0;
+  return 0;
 }
 
-int WeatherServerInstance_OnRequest(void* _Context)
-{
-	WeatherServerInstance* connected_client = (WeatherServerInstance*)_Context;
-	printf("WeatherServerInstance_OnRequest\n");
+int WeatherServerInstance_OnRequest(void *_Context) {
+  WeatherServerInstance *server = (WeatherServerInstance *)_Context;
+  printf("WeatherServerInstance_OnRequest\n");
 
-	return 0;
+  server->state = WeatherServerInstance_State_Work;
+  return 0;
 }
 
-void WeatherServerInstance_Work(WeatherServerInstance* _Server, uint64_t _MonTime)
-{
-	// Select function to run in the weather API
-	_Server->connection->readBuffer;
-	_Server->connection->method;
+void WeatherServerInstance_Work(WeatherServerInstance *_Server,
+                                uint64_t _MonTime) {
+  // Select function to run in the weather API
+  //_Server->connection->readBuffer;
+  //_Server->connection->method;
 
-	// _Server.state = WORKING
-	// State machine
-	/* If /get
-	 * If /weather
-	 * 		do A
-	 * 		get_weather("citname");
-	 * else if /surprise
-	 * 		do B
-	 *  WRITE(****)
-	 */
-	switch (_Server->state)
-	{
-		case WeatherServerInstance_State_Waiting:
-		{
-			// Wait for something to happen
-			break;
-		}
-		case WeatherServerInstance_State_Init:
-		{
-			// Initialize stuff
-			_Server->state = WeatherServerInstance_State_Work;
-			// Initialize weather API
-			break;
-		}
-		case WeatherServerInstance_State_Work:
-		{
-			// Do the work
-			// Process request
-				// If /getweather
-					// Call weather API
-				// Else if /surprise
-					// Do something else
-			break;
-		}
-		case WeatherServerInstance_State_Done:
-		{
-			// Finish up
-			_Server->state = WeatherServerInstance_State_Dispose;
-			break;
-		}
-		case WeatherServerInstance_State_Dispose:
-		{
-			// Dispose
-			break;
-		}
-		default:
-		{
-			printf("Unsupported state\n");
-			break;
-		}
-	}
+  // _Server.state = WORKING
+  // State machine
+  /* If /get
+   * If /weather
+   * 		do A
+   * 		get_weather("citname");
+   * else if /surprise
+   * 		do B
+   *  WRITE(****)
+   */
+  switch (_Server->state) {
+  case WeatherServerInstance_State_Waiting: {
+    // Wait for something to happen
+    break;
+  }
+  case WeatherServerInstance_State_Init: {
+    // Initialize stuff
+    _Server->state = WeatherServerInstance_State_Work;
+    // Initialize weather API
+    break;
+  }
+  case WeatherServerInstance_State_Work: {
+    // Do the work
+    // Process request
+    // If /getweather
+    // Call weather API
+    // Else if /surprise
+    // Do something else
+    if (strcmp(_Server->connection->url, "/health") == 0) {
+      HTTPServerConnection_SendResponse(_Server->connection, 200, "");
+    } else if (strcmp(_Server->connection->url, "/secret") == 0) {
+      HTTPServerConnection_SendResponse(_Server->connection, 501, "");
+    } else {
+      HTTPServerConnection_SendResponse(_Server->connection, 404, "");
+    }
+    _Server->state = WeatherServerInstance_State_Done;
+    break;
+  }
+  case WeatherServerInstance_State_Done: {
+    // Finish up
+    _Server->state = WeatherServerInstance_State_Dispose;
+    break;
+  }
+  case WeatherServerInstance_State_Dispose: {
+    // Dispose
+    break;
+  }
+  default: {
+    printf("Unsupported state\n");
+    break;
+  }
+  }
 
-	_Server->connection->state = DONE!;
+  _Server->state = WeatherServerInstance_State_Done;
 }
 
-void WeatherServerInstance_Dispose(WeatherServerInstance* _Instance)
-{
+void WeatherServerInstance_Dispose(WeatherServerInstance *_Instance) {}
 
-}
+void WeatherServerInstance_DisposePtr(WeatherServerInstance **_InstancePtr) {
+  if (_InstancePtr == NULL || *(_InstancePtr) == NULL)
+    return;
 
-void WeatherServerInstance_DisposePtr(WeatherServerInstance** _InstancePtr)
-{
-	if(_InstancePtr == NULL || *(_InstancePtr) == NULL)
-		return;
-
-	WeatherServerInstance_Dispose(*(_InstancePtr));
-	free(*(_InstancePtr));
-	*(_InstancePtr) = NULL;
+  WeatherServerInstance_Dispose(*(_InstancePtr));
+  free(*(_InstancePtr));
+  *(_InstancePtr) = NULL;
 }
--- libs/HTTPServer/HTTPServerConnection.c
+++ libs/HTTPServer/HTTPServerConnection.c
@@ -1,158 +1,180 @@
 #include "HTTPServerConnection.h"
-#include "HTTPParser.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 //-----------------Internal Functions-----------------
 
-void HTTPServerConnection_TaskWork(void* _Context, uint64_t _MonTime);
+void HTTPServerConnection_TaskWork(void *_Context, uint64_t _MonTime);
 
 //----------------------------------------------------
 
-int HTTPServerConnection_Initiate(HTTPServerConnection* _Connection, int _FD)
-{
-	TCPClient_Initiate(&_Connection->tcpClient, _FD);
-	
-	_Connection->url = NULL;
-	_Connection->method = NULL;
-	_Connection->bytesRead = 0;
-	_Connection->state = HTTPServerConnection_State_Init;
-	_Connection->startTime = 0;
+int HTTPServerConnection_Initiate(HTTPServerConnection *_Connection, int _FD) {
+  TCPClient_Initiate(&_Connection->tcpClient, _FD);
 
-	_Connection->task = smw_createTask(_Connection, HTTPServerConnection_TaskWork);
+  _Connection->url = NULL;
+  _Connection->method = NULL;
+  _Connection->bytesRead = 0;
+  _Connection->state = HTTPServerConnection_State_Init;
+  _Connection->startTime = 0;
+  _Connection->response = NULL;
+  _Connection->bytesSent = 0;
 
-	return 0;
+  _Connection->task =
+      smw_createTask(_Connection, HTTPServerConnection_TaskWork);
+
+  return 0;
 }
 
-int HTTPServerConnection_InitiatePtr(int _FD, HTTPServerConnection** _ConnectionPtr)
-{
-	if(_ConnectionPtr == NULL)
-		return -1;
+int HTTPServerConnection_InitiatePtr(int _FD,
+                                     HTTPServerConnection **_ConnectionPtr) {
+  if (_ConnectionPtr == NULL)
+    return -1;
 
-	HTTPServerConnection* _Connection = (HTTPServerConnection*)malloc(sizeof(HTTPServerConnection));
-	if(_Connection == NULL)
-		return -2;
+  HTTPServerConnection *_Connection =
+      (HTTPServerConnection *)malloc(sizeof(HTTPServerConnection));
+  if (_Connection == NULL)
+    return -2;
 
-	int result = HTTPServerConnection_Initiate(_Connection, _FD);
-	if(result != 0)
-	{
-		free(_Connection);
-		return result;
-	}
+  int result = HTTPServerConnection_Initiate(_Connection, _FD);
+  if (result != 0) {
+    free(_Connection);
+    return result;
+  }
 
-	*(_ConnectionPtr) = _Connection;
+  *(_ConnectionPtr) = _Connection;
 
-	return 0;
+  return 0;
 }
 
-void HTTPServerConnection_SetCallback(HTTPServerConnection* _Connection, void* _Context, HTTPServerConnection_OnRequest _OnRequest)
-{
-	_Connection->context = _Context;
-	_Connection->onRequest = _OnRequest;
+void HTTPServerConnection_SetCallback(
+    HTTPServerConnection *_Connection, void *_Context,
+    HTTPServerConnection_OnRequest _OnRequest) {
+
+  _Connection->bytesSent = 0;
+  _Connection->context = _Context;
+  _Connection->onRequest = _OnRequest;
 }
 
-void HTTPServerConnection_TaskWork(void* _Context, uint64_t _MonTime)
-{
-	HTTPServerConnection* _Connection = (HTTPServerConnection*)_Context;
+void HTTPServerConnection_SendResponse(HTTPServerConnection *_Connection,
+                                       int _responseCode, char *_responseBody) {
 
-	if(_Connection->state != HTTPServerConnection_State_Init && _MonTime - _Connection->startTime >= HTTPSERVER_TIMEOUT_MS)
-	{
-		printf("Connection timed out\n");
-		HTTPServerConnection_Dispose(_Connection);
-		return;
-	}
-
-	switch(_Connection->state)
-	{
-		case HTTPServerConnection_State_Init:
-		{
-			_Connection->startTime = _MonTime;
-			_Connection->state = HTTPServerConnection_State_Reading;
-			break;
-		}
-		case HTTPServerConnection_State_Reading:
-		{
-			TCPClient* tcpClient = &_Connection->tcpClient;
-			int read = TCPClient_Read(tcpClient, (uint8_t*)(_Connection->readBuffer + _Connection->bytesRead), READBUFFER_SIZE - _Connection->bytesRead - 1);
-
-			if(read > 0)
-			{
-				_Connection->bytesRead += read;
-				_Connection->readBuffer[_Connection->bytesRead] = '\0';
-			}
-
-			char* ret = strstr(_Connection->readBuffer, "\r\n\r\n");
-			if(ret != NULL)
-			{
-				_Connection->state = HTTPServerConnection_State_Parsing;
-			}
-
-			break;
-		}
-		case HTTPServerConnection_State_Parsing:
-		{
-			HTTPRequest* request = HTTPRequest_fromstring(_Connection->readBuffer);
-
-			_Connection->url = strdup(request->URL);
-			_Connection->method = strdup(RequestMethod_tostring(request->method));
-
-			printf("%s\n%s\n\n", _Connection->method, _Connection->url);
-			
-			HTTPRequest_Dispose(&request);
-
-			_Connection->state = HTTPServerConnection_State_Done;
-
-			break;
-		}
-		case HTTPServerConnection_State_Timeout:
-		{
-			printf("Connection timed out\n");
-			_Connection->state = HTTPServerConnection_State_Dispose;
-			break;
-		}
-		case HTTPServerConnection_State_Done:
-		{
-			if(strcmp(_Connection->method, "GET") == 0)
-			{
-				_Connection->onRequest(_Connection->context);
-			}
-			// Ska den verkligen disposea här?
-			_Connection->state = HTTPServerConnection_State_Dispose;
-			break;
-		}
-		case HTTPServerConnection_State_Dispose:
-		{
-			HTTPServerConnection_Dispose(_Connection);
-			break;
-		}
-		case HTTPServerConnection_State_Failed:
-		{
-			printf("Reading failed\n");
-			_Connection->state = HTTPServerConnection_State_Dispose;
-			break;
-		}
-		default:
-		{
-			printf("Unsupported state\n");
-			break;
-		}
-	}
-	
-	//printf("HTTPServerConnection_TaskWork\n");
+  _Connection->response = HTTPResponse_new(_responseCode, _responseBody);
+  _Connection->state = HTTPServerConnection_State_Send;
 }
 
-void HTTPServerConnection_Dispose(HTTPServerConnection* _Connection)
-{
-	TCPClient_Dispose(&_Connection->tcpClient);
-	smw_destroyTask(_Connection->task);
+void HTTPServerConnection_TaskWork(void *_Context, uint64_t _MonTime) {
+  HTTPServerConnection *_Connection = (HTTPServerConnection *)_Context;
+
+  if (_Connection->state != HTTPServerConnection_State_Init &&
+      _MonTime - _Connection->startTime >= HTTPSERVER_TIMEOUT_MS) {
+    _Connection->state = HTTPServerConnection_State_Dispose;
+  }
+
+  switch (_Connection->state) {
+  case HTTPServerConnection_State_Init: {
+    _Connection->startTime = _MonTime;
+    _Connection->state = HTTPServerConnection_State_Reading;
+    break;
+  }
+  case HTTPServerConnection_State_Reading: {
+    TCPClient *tcpClient = &_Connection->tcpClient;
+    int read = TCPClient_Read(
+        tcpClient,
+        (uint8_t *)(_Connection->readBuffer + _Connection->bytesRead),
+        READBUFFER_SIZE - _Connection->bytesRead - 1);
+
+    if (read > 0) {
+      _Connection->bytesRead += read;
+      _Connection->readBuffer[_Connection->bytesRead] = '\0';
+    }
+
+    char *ret = strstr(_Connection->readBuffer, "\r\n\r\n");
+    if (ret != NULL) {
+      _Connection->state = HTTPServerConnection_State_Parsing;
+    }
+
+    break;
+  }
+  case HTTPServerConnection_State_Parsing: {
+    HTTPRequest *request = HTTPRequest_fromstring(_Connection->readBuffer);
+
+    _Connection->url = strdup(request->URL);
+    _Connection->method = strdup(RequestMethod_tostring(request->method));
+
+    printf("%s\n%s\n\n", _Connection->method, _Connection->url);
+
+    HTTPRequest_Dispose(&request);
+
+    _Connection->state = HTTPServerConnection_State_Wait;
+
+    if (strcmp(_Connection->method, "GET") == 0) {
+      _Connection->onRequest(_Connection->context);
+    }
+
+    break;
+  }
+  case HTTPServerConnection_State_Send: {
+    if (_Connection->response == NULL) {
+      _Connection->state = HTTPServerConnection_State_Failed;
+      break;
+    }
+    char *message = (char *)HTTPResponse_tostring(_Connection->response);
+    int n = TCPClient_Write(&_Connection->tcpClient,
+                            (uint8_t *)(message + _Connection->bytesSent),
+                            strlen(message) - _Connection->bytesSent);
+    if (n > 0) {
+      _Connection->bytesSent += n;
+    }
+
+    if (_Connection->bytesSent == strlen(message)) {
+      _Connection->state = HTTPServerConnection_State_Wait;
+    }
+    free(message);
+    break;
+  }
+  case HTTPServerConnection_State_Wait: {
+    break;
+  }
+  case HTTPServerConnection_State_Timeout: {
+    printf("Connection timed out\n");
+    _Connection->state = HTTPServerConnection_State_Dispose;
+    break;
+  }
+  case HTTPServerConnection_State_Done: {
+    // Ska den verkligen disposea här?
+    _Connection->state = HTTPServerConnection_State_Dispose;
+    break;
+  }
+  case HTTPServerConnection_State_Dispose: {
+    HTTPServerConnection_Dispose(_Connection);
+    break;
+  }
+  case HTTPServerConnection_State_Failed: {
+    printf("Reading failed\n");
+    _Connection->state = HTTPServerConnection_State_Dispose;
+    break;
+  }
+  default: {
+    printf("Unsupported state\n");
+    break;
+  }
+  }
+
+  // printf("HTTPServerConnection_TaskWork\n");
 }
 
-void HTTPServerConnection_DisposePtr(HTTPServerConnection** _ConnectionPtr)
-{
-	if(_ConnectionPtr == NULL || *(_ConnectionPtr) == NULL)
-		return;
+void HTTPServerConnection_Dispose(HTTPServerConnection *_Connection) {
+  TCPClient_Dispose(&_Connection->tcpClient);
+  if (_Connection->response)
+    HTTPResponse_Dispose(&_Connection->response);
+  smw_destroyTask(_Connection->task);
+}
 
-	HTTPServerConnection_Dispose(*(_ConnectionPtr));
-	free(*(_ConnectionPtr));
-	*(_ConnectionPtr) = NULL;
+void HTTPServerConnection_DisposePtr(HTTPServerConnection **_ConnectionPtr) {
+  if (_ConnectionPtr == NULL || *(_ConnectionPtr) == NULL)
+    return;
+
+  HTTPServerConnection_Dispose(*(_ConnectionPtr));
+  free(*(_ConnectionPtr));
+  *(_ConnectionPtr) = NULL;
 }
