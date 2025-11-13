dd2db2e08d6931495f5353d9c33223c69bc84451
Gustav Frisén <gustav.frisen@outlook.com>
2025-11-13 09:19:18 +0100

    Moved definition of readFileToBuffer() from utils.h to utils.c

--- libs/utils.c
+++ libs/utils.c
@@ -14,3 +14,24 @@ uint64_t SystemMonotonicMS() {
 
   return result;
 }
+
+uint8_t* readFileToBuffer(const char* path, size_t* outSize) {
+    FILE* f = fopen(path, "rb");
+    if (!f) return NULL;
+
+    fseek(f, 0, SEEK_END);
+    long size = ftell(f);
+    fseek(f, 0, SEEK_SET);
+
+    uint8_t* buffer = malloc(size);
+    if (!buffer) {
+        fclose(f);
+        return NULL;
+    }
+
+    fread(buffer, 1, size, f);
+    fclose(f);
+
+    *outSize = size;
+    return buffer;
+}
--- libs/utils.h
+++ libs/utils.h
@@ -9,25 +9,6 @@
 
 uint64_t SystemMonotonicMS();
 
-uint8_t* readFileToBuffer(const char* path, size_t* outSize) {
-    FILE* f = fopen(path, "rb");
-    if (!f) return NULL;
-
-    fseek(f, 0, SEEK_END);
-    long size = ftell(f);
-    fseek(f, 0, SEEK_SET);
-
-    uint8_t* buffer = malloc(size);
-    if (!buffer) {
-        fclose(f);
-        return NULL;
-    }
-
-    fread(buffer, 1, size, f);
-    fclose(f);
-
-    *outSize = size;
-    return buffer;
-}
+uint8_t* readFileToBuffer(const char* path, size_t* outSize);
 
 #endif
